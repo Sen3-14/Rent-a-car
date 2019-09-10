c1315ef971b00f9aa6cb39b110d6a5aeedf9cbd5
A <A>
2019-09-10 17:38:23 +0200

    header

--- admin/menu.php
+++ admin/menu.php
@@ -3,7 +3,7 @@
       if(isset($_SESSION['status-admin']) == 1){
 ?>
 <div id="top">
-			<h1><a href="#">Rent-a-car sistem</a></h1>
+			<h1><a href="#">Administrator</a></h1>
 			<div id="top-navigation">
 				Zdravo <a href="#"><strong><?php echo $_SESSION['user']; ?></strong></a>
 				<span>|</span>
@@ -12,7 +12,6 @@
 		</div>
 <div id="navigation">
 			<ul>
-			    <li><a href="index.php"><span>Meni</span></a></li>
 			    <li><a href="add_vehicles.php"><span>Upravljanje vozilima</span></a></li>
 			    <li><a href="client_requests.php"><span>Zahtevi za zakazivanje</span></a></li>
 			    <li><a href="index.php"><span>Poruke</span></a></li>
