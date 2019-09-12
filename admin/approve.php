7f773fbd1ddc0ec1d3ded822876c962b8fb87ae7
A <A>
2019-09-12 21:33:03 +0200

    dule

--- admin/approve.php
+++ admin/approve.php
@@ -3,8 +3,20 @@
 	$id = $_REQUEST['id'];
 	$query = "UPDATE client SET status = 'Approved' WHERE client_id = '$id'";
 	$result = $conn->query($query);
-	if($result === TRUE){
-		echo 'Zahtev je uspešno potvrđen.';
+	
+	$qry = "SELECT * FROM client";
+	$rslt = $conn->query($qry);
+    $row = mysqli_fetch_assoc($rslt);
+	
+
+	$query1 = "UPDATE hire SET status = 'Approved' WHERE client = '$row[email]'";
+	$result1 = $conn->query($query1);
+
+		if($result == TRUE && $result1 == TRUE){
+		echo "<script type = \"text/javascript\">
+		alert(\"Status promenjen.\");
+		window.location = (\"client_requests.php\")
+		</script>";
 	?>
 		<meta content="4; client_requests.php" http-equiv="refresh" />
 	<?php
