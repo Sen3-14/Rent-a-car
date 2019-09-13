6bfbb948f9c7c3d624c2252c3f46187d3e6dc800
A <A>
2019-09-14 00:02:52 +0200

    search sistem za poruke

---
+++ admin/fetch.php
@@ -0,0 +1,58 @@
+<?php
+ include '../includes/config.php';
+$output = '';
+
+if(isset($_POST["query"]))
+{
+    $search = $_POST["query"];
+	$query = "SELECT client.fname,client.lname,client.email,message.client_id,message.msg_id,message.message,message.status,message.time FROM client JOIN message ON client.email=message.client_id
+	WHERE client.fname LIKE '%".$search."%'
+	OR client.lname LIKE '%".$search."%' 
+	OR client.email LIKE '%".$search."%' 
+	";
+} else {
+    $query = "SELECT client.fname,client.lname,client.email,message.client_id,message.msg_id,message.message,message.status,message.time FROM client JOIN message ON client.email=message.client_id ORDER BY 'time'";
+}
+
+$result = $conn->query($query);
+
+if(mysqli_num_rows($result) > 0)
+{
+	$output .= '<div class="table-responsive">
+					<table class="table table bordered" style="text-align: center;">
+					<form action="sendReply.php" method="POST">
+					<tr>
+							<th>Ime</th>
+							<th>Prezime</th>
+                            <th>E-mail</th>
+							<th>Poruka</th>
+							<th>Vreme</th>
+							<th>Status</th>
+							<th>Kontrole</th>
+						</tr>';
+	while($row = mysqli_fetch_array($result))
+	{
+		$output .= '
+			<tr>
+				<td>'.$row["fname"].'</td>
+				<td>'.$row["lname"].'</td>
+				<td>'.$row["email"].'</td>
+				<td>'.$row["message"].'</td>
+				<td>'.$row["time"].'</td>
+				<td>'.$row["status"].'</td>
+				<td>
+				<a href="javascript:sureToApprove('.$row["msg_id"].')" class="ico del">Obriši</a>
+				<a href="javascript:changeStatus('.$row["msg_id"].')" class="ico edit">Označi</a>
+				<a href="javascript:sendMessage('.$row["msg_id"].')" class="add-button"><span>Pošalji odgovor</span></a>
+				</td>
+			</tr>
+			</form>
+		';
+	}
+	echo $output;
+}
+else
+{
+	echo 'Data Not Found';
+}
+?>
--- admin/index.php
+++ admin/index.php
@@ -5,26 +5,11 @@
 <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <title>Administrator</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
-    <script type="text/javascript">
-    function sureToApprove(id) {
-        if (confirm("Da li ste sigurni da zelite da izbrišete poruku?")) {
-            window.location.href = 'delete_msg.php?id=' + id;
-        }
-    }
-    function changeStatus(id){
-         if(confirm("Promeni u pročitano?")){
-                window.location.href = 'changeStatus.php?id=' +id;
-         }
-    }
-    function sendMessage(id){
-        if(!id){
-            window.alert("Izaberite poruku kojoj šaljete odgovor");
-        } else {
-            window.location.href = 'sendMessage.php?id=' +id;
-        }
-    }
-    </script>
+    
 </head>
 
 <body>
@@ -38,76 +23,27 @@ include 'menu.php';
         </div>
     </div>
 
-    <div id="container">
-        <div class="shell">
-
-            <div class="small-nav">
-                <a href="index.php">Komandna tabla</a>
-                <span>&gt;</span>
-                Poruke klijenata
-            </div>
-
-            <br />
-
-            <div id="main">
-                <div class="cl">&nbsp;</div>
-
-                <div id="content">
-
-                    <div class="box">
-                        <div class="box-head">
-                            <h2 class="left">Poruke klijenata</h2>
-                            <div class="right">
-                                <input type="text" class="field small-field" />
-                                <input type="submit" class="button" value="pretraga" />
-                            </div>
-                        </div>
-
-                        <div class="table">
-                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
-                                <tr>
-                                    <th>Poruka</th>
-                                    <th>Vreme</th>
-                                    <th>Status</th>
-                                    <th width="150" class="ac">Kontrole</th>
-                                </tr>
-                                <?php
-include '../includes/config.php';
-$select = "SELECT * FROM message";
-$result = $conn->query($select);
-while ($row = $result->fetch_assoc()) {
-    ?>                         <form action="sendReply.php" method="POST">
-                                <tr>
-                                    <td>
-                                       <?php echo $row['message'] ?>
-                                    </td>
-                                    <td style="text-align:center;"><?php echo $row['time']?></td>
-                                    <td style="text-align:center;"><?php echo $row['status']?></td>
-                                    <td style="text-align:center;">
-                                 <a href="javascript:sureToApprove(<?php echo $row['msg_id']; ?>)"
-                                            class="ico del">Obriši</a>
-                                <a href="javascript:changeStatus(<?php echo $row['msg_id']; ?>)" class="ico edit">Označi</a></br>
-                            <a href="javascript:sendMessage(<?php echo $row['msg_id'];?>)" class="add-button"><span>Pošalji odgovor</span></a>
-                            <div class="cl">&nbsp;</div>
-                                    </td>
-                                </tr>
-                            </form>
-                                <?php
-}
-?>
-                            </table>
-
-                        </div>
-                        <br><h2><input type="submit" onclick="window.print()" value="Štampaj" /></h2>
-
-                    </div>
-
-                </div>
-
-                <div class="cl">&nbsp;</div>
+    <div class="container">
+        <br />
+        <br />
+        <br />
+        <h2 align="center">Poruke klijenata</h2><br />
+        <div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon">Search</span>
+                <input type="text" name="search_text" id="search_text" placeholder="Trazi po imenu,prezimenu ili e-mailu" class="form-control" />
             </div>
         </div>
+        <br />
+        <div id="result"></div><h2><input type="submit" onclick="window.print()" value="Štampaj" /></h2>
     </div>
+    <div style="clear:both"></div>
+    <br />
+    
+    <br />
+    <br />
+    <br />
+
     <div id="footer">
         <div class="shell">
             <span class="left">&copy; <?php echo date("Y"); ?></span>
@@ -119,3 +55,49 @@ while ($row = $result->fetch_assoc()) {
 </body>
 
 </html>
+
+<script>
+    $(document).ready(function () {
+        load_data();
+        function load_data(query) {
+            $.ajax({
+                url: "fetch.php",
+                method: "POST",
+                data: { query: query },
+                success: function (data) {
+                    $('#result').html(data);
+                }
+            });
+        }
+
+        $('#search_text').keyup(function () {
+            var search = $(this).val();
+            if (search != '') {
+                load_data(search);
+            }
+            else {
+                load_data();
+            }
+        });
+    });
+</script>
+
+<script type="text/javascript">
+    function sureToApprove(id) {
+        if (confirm("Da li ste sigurni da zelite da izbrišete poruku?")) {
+            window.location.href = 'delete_msg.php?id=' + id;
+        }
+    }
+    function changeStatus(id) {
+        if (confirm("Promeni u pročitano?")) {
+            window.location.href = 'changeStatus.php?id=' + id;
+        }
+    }
+    function sendMessage(id) {
+        if (!id) {
+            window.alert("Izaberite poruku kojoj šaljete odgovor");
+        } else {
+            window.location.href = 'sendMessage.php?id=' + id;
+        }
+    }
+</script>
