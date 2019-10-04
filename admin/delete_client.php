94f362f677de31f7efe2cccd2d6e93e2d71066f6
jovan <jovan>
2019-10-04 22:55:39 +0200

    finalna mozda

--- admin/delete_client.php
+++ admin/delete_client.php
@@ -1,10 +1,29 @@
 <?php
 	include '../includes/config.php';
     $id = $_REQUEST['id'];
-    
+
+		$qry1 = "SELECT * FROM client WHERE client_id = '$id'";
+    $sql = $conn->query($qry1);
+    $result = mysqli_fetch_assoc($sql);
+		$vozilo = $result['car_id'];
+		if($vozilo !== NULL) {
+		$qry4 = "SELECT * FROM cars WHERE car_id='$vozilo'";
+		$sqlquery = $conn->query($qry4);
+		$row = mysqli_fetch_assoc($sqlquery);
+			$a=$row['availability'];
+			if ($a == 0) {
+				$qrr = "UPDATE cars SET status = 'Available'";
+				$qrr1 = $conn->prepare($qrr1);
+				$qrr1->execute();
+			}
+		$b = ++$a;
+			$qr = "UPDATE cars SET availability = $b WHERE car_id='$vozilo'";
+		$query4 = $conn->prepare($qr);
+		$query4->execute(); }
+
     $qry1 = "DELETE FROM client WHERE client_id = '$id'";
     $sql = $conn->query($qry1);
-    
+
     if($sql){
         echo "<script type = \"text/javascript\">
                         alert(\"Klijent izbisan.\");
@@ -14,3 +33,4 @@
     } else {
         exit();
     }
+?>
